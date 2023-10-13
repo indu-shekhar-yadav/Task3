48b9b0514deede731ba54b6283db310877b2f978
Indu Shekhar Yadav <shekharindu21@gmail.com>
2023-10-13 21:54:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const colors = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#33FFFF', '#808080', '#FFA500', '#0000FF', '#800080', '#FF0000', '#008080', '#FF1493', '#00FF7F', '#7B68EE', '#FFD700', '#40E0D0', '#800000'];
+
 function App() {
+  const [boxColor, setBoxColor] = useState('#FFFFFF'); 
+  const [showColorSquares, setShowColorSquares] = useState(false);
+
+  const handleSquareClick = (color) => {
+    setBoxColor(color); 
+  };
+
+  const toggleColorSquares = () => {
+    setShowColorSquares(!showColorSquares);
+  };
+
+  const ColorSquare = ({ color, onClick }) => {
+    return (
+      <div
+        className="color-square"
+        style={{ backgroundColor: color }}
+        onClick={() => onClick(color)}
+      ></div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: '#586EC2' }}>
+      
+      <div className={`big-rounded-box`} style={{ backgroundColor: boxColor }}>
+        <h1>Click to Change the Color</h1>
+        <div className="color-squares">
+          {colors.map((color, index) => (
+            <ColorSquare key={index} color={color} onClick={handleSquareClick} />
+          ))}
+        </div>
+        <h2>Background Color: {boxColor}</h2>
+      </div>
     </div>
   );
 }
